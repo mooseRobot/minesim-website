573ec177020bdbb6decfc7b1bc74f34aebd8f81b
Felix <111679444+mooseRobot@users.noreply.github.com>
2023-09-13 19:33:31 -1000

    Add terms of service content

--- frontend/src/pages/terms.js
+++ frontend/src/pages/terms.js
@@ -1,11 +1,200 @@
 import { React } from 'react';
+import './legal.css'
 
 function Terms () {
     return (
-        <div  className={'legal'}>
+        <div className={'legal-content'}>
             <div className={'legal-title'}>
                 <h1>Terms Of Service</h1>
             </div>
+            <div className={'legal-intro'}>
+                <p>Last updated: September 13th, 2023</p>
+                <p>These terms apply to the following domain and all of its subdomains.</p>
+                <br></br>
+                <p>
+                    Welcome to MineSim (the "Bot"), a Discord bot that allows users to play games within Discord. These
+                    terms of service (the "Terms") govern your access to and use of the Bot, so please read them
+                    carefully before using the Bot.
+                </p>
+                <p>
+                    By accessing or using the Bot, you agree to be bound by these Terms and our
+                    <a href={'https://github.com/mooseRobot/MineSim/blob/main/PRIVACY.md'}> Privacy Policy</a>. If you
+                    do not agree to these Terms or the Privacy Policy, you may not access or use the Bot.
+                </p>
+                <p>
+                    Using MineSim is at-will and can be terminated at any time by either us or you for any reason.
+                    By using the MineSim Bot, you agree to be bound by these TOU. If you do not agree to be so bound,
+                    you are not authorized to use the Bot. These TOU are a legal contract between you and MineSim and
+                    govern your access to and use of the Bot together with any services offered through the Bot.
+                </p>
+                <p>MineSim is not affiliated with Discord.</p>
+            </div>
+
+            <section>
+                <article>
+                    <h2>MineSim</h2>
+                    <p>
+                        By using the Bot, you may need to interact with other Bot users. You are solely responsible for
+                        any such interaction and agree to do so in a manner that is legal, respectable, and consistent
+                        with these TOU. MineSIm is not responsible for the conduct of any other user who may interact
+                        with you, regardless of whether or not it is done through MineSim's own Discord server.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>User Conduct</h2>
+                    <p>
+                        You agree to use the Bot only for lawful purposes and in accordance with these Terms. You agree
+                        not to use the Bot:
+                    </p>
+                    <ul>
+                        <li>To harass, bully, or spam other users.</li>
+                        <li>
+                            To post or transmit any content that is defamatory, libelous, obscene, pornographic, or
+                            otherwise inappropriate.
+                        </li>
+                        <li>
+                            To post or transmit any content that infringes the rights of any third party, including
+                            without limitation intellectual property rights.
+                        </li>
+                        <li>
+                            To post or transmit any unsolicited or unauthorized advertising, promotional materials,
+                            "junk mail," "spam," "chain letters," "pyramid schemes," or any other form of solicitation.
+                        </li>
+                        <li>
+                            To impersonate any person or entity, or falsely state or otherwise misrepresent your
+                            affiliation with a person or entity.
+                        </li>
+                        <li>
+                            To interfere with or disrupt the operation of the Bot or servers or networks connected to
+                            the Bot, or violate any requirements, procedures, policies, or regulations of such networks.
+                        </li>
+                    </ul>
+                    <p>
+                        We reserve the right, in our sole discretion, to terminate or suspend your access to the Bot at
+                        any time and for any reason without notice.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>License</h2>
+                    <p>
+                        Subject to your compliance with these Terms, we grant you a limited, non-exclusive,
+                        non-transferable, revocable license to access and use the Bot for your personal, non-commercial
+                        use.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>Content Disclaimers</h2>
+                    <p>
+                        MineSim does not warrant or guarantee the accuracy, completeness, or validity of the
+                        recommendations and data distributed by the Bot. In no event shall MineSim be liable to you or
+                        anyone else for any decision made or action taken by you in reliance of any information or data
+                        found on the Site or through the Bot.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>Refunds</h2>
+                    <p>MineSim does not offer any refunds for Premium subscriptions.</p>
+                </article>
+
+                <article>
+                    <h2>Intellectual and Other Property</h2>
+                    <p>Certain elements of MineSim, including but not limited to, graphics, color selections,
+                        organization and layout, are copyright protected under United States of America and
+                        international copyright laws and treaty provisions. Any MineSim Content protected by
+                        intellectual property laws may not be copied, republished, posted, modified, edited,
+                        transmitted, distributed, used to create derivative works of, or reverse engineered without
+                        permission, except that you may print out one copy of each Site page solely for noncommercial
+                        personal or educational use. No right, title, or interest in any MineSim Content is transferred
+                        to you as a result of you accessing, downloading, or printing such content from the Site or Bot.
+                        Any use of MineSim Content must display the appropriate copyright, trademark, and other
+                        proprietary notices.
+                    </p>
+                    <p>
+                        You acknowledge that you have no right, title, or interest in or to MineSim and/or MineSim
+                        Content.
+                    </p>
+                    <p>
+                        MineSim and the MineSim logo are marks of MineSim. Other marks, names, and logos on the Site and
+                        Bot are the property of their respective owners.
+                    </p>
+                    <p>
+                        There may be other content located on the Site and on the bot not owned by MineSim, and you
+                        should respect those property rights as well. All rights not expressly granted herein are
+                        reserved to MineSim.
+                    </p>
+                    <p>
+                        In the event that any owners of intellectual property used by MineSim wish for their assets to
+                        be removed or have any concerns, we encourage them to reach out to us at
+                        <a href={'mailto:admin@minesim.net'}> admin@minesim.net</a>. We are committed to addressing such
+                        concerns promptly and complying with any reasonable requests to remove copyrighted materials.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>Limitation of Liability</h2>
+                    <p>
+                        If you find that you are not satisfied with the terms of use, the bot, or any goods or services
+                        offered through the bot, we kindly suggest that you stop using the bot as your exclusive remedy.
+                    </p>
+                    <p>
+                        You are responsible for following your applicable local laws and determining, among other
+                        things, whether your use of the Bot violates any of those local laws. By using the Bot, you
+                        agree and acknowledge that information about you, including personally your discord ID, and
+                        gender, may be transmitted to and stored.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>Miscellaneous</h2>
+                    <p>
+                        You acknowledge that the opinions and recommendations contained on the Server are not necessarily
+                        those of MineSim nor endorsed by MineSim. Any reliance on any opinions or recommendations
+                        offered on the Server is done at your risk. MineSim does not guarantee or promise that any
+                        opinions and/or recommendations on the Bot are accurate or will be helpful to any issue you may
+                        have. You agree that MineSim is not liable to you or anyone else for any harm that might arise
+                        as a result of using and/or implementing in any manner any of the opinions or recommendations
+                        found on the Bot.
+                    </p>
+                    <p>
+                        If any part of these TOU is determined by a court of competent jurisdiction to be invalid or
+                        unenforceable, it will not impact any other provision of these TOU, all of which will remain in
+                        full force and effect.
+                    </p>
+                    <p>
+                        These TOU constitute the entire agreement of the parties with respect to the Bot and supersede
+                        all prior communications, promises and proposals, whether oral, written, or electronic, between
+                        you and MineSim, with respect to the Bot.
+                    </p>
+                    <p>
+                        If you violate any portion of these TOU, MineSim reserves the right, without an obligation to do
+                        so, to deny you access to the Bot and/or remove any UGC you may have posted/uploaded on the
+                        Site.
+                    </p>
+                    <p>MineSim’s failure to enforce any portion of these TOU is not a waiver of such portion.</p>
+                    <p>
+                        The proprietary rights, disclaimer of warranties, representations made by you, indemnities, and
+                        limitations of liability shall survive the termination of these TOU.
+                    </p>
+                    <p>
+                        MineSim reserves the right, without notice and reason, to take down or terminate the Bot or
+                        otherwise revoke any and all access granted to you related to the Bot. You agree that MineSim is
+                        not liable to you or any other third party for this action.
+                    </p>
+                    <p>
+                        You are free to link to the Bot long as there is nothing deceptive or infringing about the link.
+                        MineSim may revoke this linking permission at any time and for any reason.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>Changes to These Terms and Conditions</h2>
+                    <p>We reserve the right to change these terms of service at any time without any notice.</p>
+                </article>
+            </section>
         </div>
     )
 }
