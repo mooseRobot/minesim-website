83d2d2a581d0d3dea4d51db584a69cec724c2d8d
Felix <111679444+mooseRobot@users.noreply.github.com>
2023-09-13 15:04:34 -1000

    Remove filler text and add content

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -12,7 +12,7 @@ function About () {
             <div className="content-top padding-15">
                 <div className="content-top-left">
                     <p className="title">MineSim</p>
-                    <p className="top-subtext">MineSim is so awesome that it has subtext that goes under the main title. This subtext will probably describe the bot. Invite now. Longer description, I am typing out a longer description to fill out this empty space.</p>
+                    <p className="top-subtext"><b>Join thousands of users</b> around the world in Discord's most fun economic bot. With a dynamic in-game economy and a plethora of features, it is designed to offer endless opportunities for fun and competition. Invite now to embark on a journey to riches and glory</p>
                     <a className="invite-button" target="_blank" rel="noopener noreferrer" href="https://discord.com/oauth2/authorize?client_id=1043441355847901214&permissions=1093606571088&scope=bot%20applications.commands">
                         Click to Invite
                     </a>
@@ -20,11 +20,11 @@ function About () {
                 <img alt="minesim splash" className="content-top-picture" src={minesimsplash} />
             </div>
 
-            <p className="content-info-title">Some Random information.</p>
+            <p className="content-info-title">At a Glance</p>
             <div className="content-info">
                 <div className="content-info-container">
                     <img alt="anime girl" src={anime} className="random-info-icon" width="150" />
-                    <p className="content-info-text">Our Mascot</p>
+                    <p className="content-info-text">Meet our charming mascot</p>
                 </div>
 
                 <div className="content-info-container">
@@ -39,7 +39,7 @@ function About () {
 
                 <div className="content-info-container">
                     <img alt="town" src={town} width="150" className="random-info-icon" />
-                    <p className="content-info-text">Build and upgrade your town</p>
+                    <p className="content-info-text">Your pathway to riches</p>
                 </div>
             </div>
 
@@ -51,8 +51,8 @@ function About () {
             <div className="call-to-action">
                 <div className="banner">
                     <div className="call-to-action-text">
-                        <p className="call-to-action-title">Call to action! It's time to start mining!</p>
-                        <p className="call-to-action-subtext">Invite the bot now and start mining. The button is over there!</p>
+                        <p className="call-to-action-title">Ready to Strike it Rich? Start Mining Now!</p>
+                        <p className="call-to-action-subtext">Click 'Invite' to bring MineSim to your server. Your adventure awaits!</p>
                     </div>
                     <a className="invite-button call-to-action-button" target="_blank" rel="noopener noreferrer" href="https://discord.com/oauth2/authorize?client_id=1043441355847901214&permissions=1093606571088&scope=bot%20applications.commands">
                         Click to Invite
