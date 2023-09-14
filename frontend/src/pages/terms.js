4b5d99b3b7315e08e94fcd3a4405f1fbc3466f35
Felix <111679444+mooseRobot@users.noreply.github.com>
2023-09-13 16:39:34 -1000

    Create terms page

---
+++ frontend/src/pages/terms.js
@@ -0,0 +1,13 @@
+import { React } from 'react';
+
+function Terms () {
+    return (
+        <div  className={'legal'}>
+            <div className={'legal-title'}>
+                <h1>Terms Of Service</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Terms;
